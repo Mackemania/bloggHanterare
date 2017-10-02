ccfc4e58e6d81621cd57de1dad59718aca08ef16
Markus Matiasson <markus.matiasson@learnet.se>
2017-10-02 14:22:12 +0200

    Index created

---
+++ index.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Startsida | Bibliotekssystem</title>
+		<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
+        <script src="code.js"></script>
+        <script src="ajaxlib.js"></script>
+	</head>
+	<body>
+
+		<div id="container">
+        </div>
+	</body>
+
+</html>

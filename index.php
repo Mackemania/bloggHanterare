a7dbea2328b5687787b621f350021210556bb60c
Andreas Andersson <andreas.andersson14@learnet.se>
2017-10-05 09:42:21 +0200

    Fixat en regform, skapat css.

--- index.php
+++ index.php
@@ -1,14 +1,25 @@
 <html>
 	<head>
 		<title>Startsida | Bibliotekssystem</title>
-		<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
+		<link href="blog_style.css" media="screen" type="text/css" rel="stylesheet"/>
         <script src="code.js"></script>
         <script src="ajaxlib.js"></script>
 	</head>
 	<body>
 
 		<div id="container">
-        </div>
+<h2>Logga in</h2>
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  Användaramn: <input type="text" placeholder="Användarnamn..." autocomplete="off" name="username" required="required">
+  <br><br>
+  E-mail: <input type="email" maxlength="50" placeholder="tex: epost@hemsida.se" autocomplete="off" name="email" required="required">
+  <br><br>
+  Lösenord <input type="password" autocomplete="off" name="password" required="required">
+  <br><br>
+
+  <input type="submit" name="Submit" value="Logga in">
+</form>
+				</div>
 	</body>
 
 </html>

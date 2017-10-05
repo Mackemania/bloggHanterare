4839ba9a0373c966ba6d795949d762dec842bc3f
Markus Matiasson <markus.matiasson@learnet.se>
2017-10-05 10:14:44 +0200

    Lade till en ny font, lade till en menyfil och en loggainfil

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <html>
 	<head>
 		<title>Startsida | Bibliotekssystem</title>
-		<link href="style.css" media="screen" type="text/css" rel="stylesheet"/>
+		<link href="blog_style.css" media="screen" type="text/css" rel="stylesheet"/>
         <script src="code.js"></script>
         <script src="ajaxlib.js"></script>
 	</head>
 	<body>
+		<?PHP
+        	require_once("blog_menu.php");
+        ?>
 
 		<div id="container">
         </div>

d035a903ea38ef5b4aaafe6f8f749fbaa688b405
Andreas Andersson <andreas.andersson14@learnet.se>
2017-10-05 10:17:29 +0200

    Ändringar i indexfilen

--- index.php
+++ index.php
@@ -8,17 +8,7 @@
 	<body>
 
 		<div id="container">
-<h2>Logga in</h2>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-  Användaramn: <input type="text" placeholder="Användarnamn..." autocomplete="off" name="username" required="required">
-  <br><br>
-  E-mail: <input type="email" maxlength="50" placeholder="tex: epost@hemsida.se" autocomplete="off" name="email" required="required">
-  <br><br>
-  Lösenord <input type="password" autocomplete="off" name="password" required="required">
-  <br><br>
 
-  <input type="submit" name="Submit" value="Logga in">
-</form>
 				</div>
 	</body>
 

66d20cd64fb85c101f03036d781b176e2a62adf8
Andreas Andersson <andreas.andersson14@learnet.se>
2017-10-10 14:52:44 +0200

    Lagt till Bootstrap och fixat i HTML koden med utseendet

--- blog_blogMaker.php
+++ blog_blogMaker.php
@@ -1,7 +1,7 @@
 <form action='blog_createBlog.php' method='post'>
   Name of the blog: <input type='text' name='blogName'><br/>
   Description of the blog: <input type='text' name='blogDescription'><br/>
-  <input type='submit' value='Gör blog'><br/>
+  <input type='submit' value='Skapa blogg'><br/>
 </form>
 <?php
 ?>

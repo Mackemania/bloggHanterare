dbe0a3c113414ff30a0de2b6c96f8191a02aeae8
Markus Matiasson <markus.matiasson@learnet.se>
2017-10-11 14:01:39 +0200

    Fixade en bugg och skapa de en sida som ska visa en egna bloggar

--- blog_blogLink.php
+++ blog_blogLink.php
@@ -1,17 +1,21 @@
 //gör en lista med alla bloggar.
 <?php
-  $blogLink = "";
-  $blogName = "";
+    $blogLink = "";
+    $blogName = "";
 
 
+<<<<<<< Updated upstream
   require_once("blog_db.php");
   $db = new DB();
+=======
+    require_once("blog_db.php");
+    $db = new DB("localhost", "root", "", "blog");
+>>>>>>> Stashed changes
 
-  $sql = "SELECT blogID, blogTitle FROM blog";
-  $matrix = $db->getData($sql);
+    $sql = "SELECT blogID, blogTitle FROM blog";
+    $matrix = $db->getData($sql);
 
-  for($i = 0; $i<count($matrix); $i++)
-  {
-    echo "<a href=blog/blog_".$matrix[$i][0]."/blog.php>".$matrix[$i][1]."</a><br/>";
-  }
+    for($i = 0; $i<count($matrix); $i++) {
+        echo "<a href=blog/blog_".$matrix[$i][0]."/blog.php>".$matrix[$i][1]."</a><br/>";
+    }
 ?>

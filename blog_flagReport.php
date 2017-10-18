6494954dfd3f0d79ed147882cdce9af22b3c42c5
Martin Pirklbauer Svensson <martin-1@live.se>
2017-10-18 15:08:08 +0200

    lade till så att man kan rapporta kommentarer

--- blog_flagReport.php
+++ blog_flagReport.php
@@ -6,22 +6,30 @@ session_start();
 $userID = $_SESSION["userID"];
 
 $blogID = $_SESSION["blogID"];
-$postID = $_REQUEST["postID"];
 
+if(isset($_REQUEST["postID"])) {
+    $postID = $_REQUEST["postID"];
+    $reportFlag = "INSERT INTO flag(userID, blogID, postID) VALUES ($userID, $blogID, $postID)";
+    $db->execute($reportFlag);
+}
+
+
+
+if(isset($_REQUEST["commentID"])) {
+    $commentID = $_REQUEST["commentID"];
+    $reportFlag = "INSERT INTO flag(userID, blogID, commentID) VALUES ($userID, $blogID, $commentID)";
+    $db->execute($reportFlag);
+}
 
 //$sql = "SELECT userID FROM user WHERE userID=$userID";
 
 //$matrix = $db->getData($sql);
 
-$reportFlag = "INSERT INTO `flag`(`userID`, `blogID`, `postID`) VALUES ($userID, $blogID, $postID)";
 
-$db->execute($reportFlag);
 
 /*if(isset($userID)){
 
 }
 */
-
-
 
 ?>
